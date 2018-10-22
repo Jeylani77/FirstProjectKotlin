e7598cbbcf122cadc1a639d807c0a63fec760aaa
akn <khadrenjaay@gmail.com>
2018-10-22 20:43:57 +0000

    first commit

---
+++ src/demo.kt
@@ -0,0 +1,16 @@
+@file: JvmName("Demo")
+
+fun main(args : Array<String>){
+
+    var name : String = "Jeylani"
+
+    var array : Array<Int> = arrayOf(1,2,3)
+
+    println(" ")
+    println("Nom : $name")
+
+    for (x in array) println(x)
+    
+}
+
+
